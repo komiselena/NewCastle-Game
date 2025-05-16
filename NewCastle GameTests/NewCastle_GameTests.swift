fa9868f6b99dc5bdbd7e5a201218173c4265ab39
komiselena <komiselen@yandex.ru>
2025-05-16 20:34:56 +0300

    Initial Commit

---
+++ NewCastle GameTests/NewCastle_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewCastle_GameTests.swift
+//  NewCastle GameTests
+//
+//  Created by Mac on 16.05.2025.
+//
+
+import Testing
+@testable import NewCastle_Game
+
+struct NewCastle_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ NewCastle GameUITests/NewCastle_GameUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NewCastle_GameUITestsLaunchTests.swift
+//  NewCastle GameUITests
+//
+//  Created by Mac on 16.05.2025.
+//
+
+import XCTest
+
+final class NewCastle_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
